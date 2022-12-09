2ee185081e653376bb5c167573fe1bc912aef12e
gyuboone <gyuboone@naver.com>
2022-12-09 01:53:59 +0000

    testtt

--- code/train.py
+++ code/train.py
@@ -30,9 +30,9 @@ def parse_args():
 
     parser.add_argument('--image_size', type=int, default=1024)
     parser.add_argument('--input_size', type=int, default=512)
-    parser.add_argument('--batch_size', type=int, default=12)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
-    parser.add_argument('--max_epoch', type=int, default=200)
+    parser.add_argument('--max_epoch', type=int, default=50)
     parser.add_argument('--save_interval', type=int, default=5)
 
     args = parser.parse_args()
