1d51daceda06ae130f79033ced52ff1d9f192841
BaeSH <65005479+byj9402@users.noreply.github.com>
2022-12-11 21:50:03 +0900

    Feat5 (#15)
    * feat: fix train.py
    
    * feat: add print F1 score
    
    #5
    
    * feat: add .gitignore
    
    * fix: train.py
    
    * fix: inference.py
    
    * fix: train_script.sh

--- code/train_script.sh
+++ code/train_script.sh
@@ -4,6 +4,6 @@ python train.py --image_size 1024 \
 --batch_size 32 \
 --learning_rate 0.001 \
 --max_epoch 200 \
---save_interval 1 \
+--save_interval 10 \
 --start_early_stopping 30 \
 --early_stopping_patience 10
