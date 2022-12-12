256b7d454fcc75a2a3383065f9a553b23c941a0b
GuSangmo <gusangmo0605@gmail.com>
2022-12-12 09:06:58 +0000

    feat: implement checkpoint and ai_hub download script
     #19
     #20

--- code/base.py
+++ code/base.py
@@ -6,6 +6,9 @@ TOKEN_TO_PATH = {
     "full17": "ICDAR17" , 
     "full19" : "ICDAR19",
     "camper" : "boostcamp" , 
-    "aihub"  : "aihub"}
+    "aihub"  : "aihub"     ,
+    "aihub_small" : "aihub_small" , 
+    "aihub_test" : "aihub_test_100" 
+    }
 
 DATASETS_TO_USE = ["ko17", "full19"]
